73888e03728ce2c4079077a6f6939a2874746673
Franz Benthin <fbenthin@mevango.com>
2016-10-09 22:45:48 +0200

    add custom footer element

---
+++ src/components/CustomFooter.vue
@@ -0,0 +1,41 @@
+<!------------------- H T M L ------------------->
+
+<template>
+  <div class="footer">
+    footer element
+  </div>
+</template>
+
+
+<!----------------- S C R I P T ----------------->
+
+<script>
+
+  //  import S from './SongPlayer'
+  //  import dummy from './../dummy'
+
+  export default {
+    props: [],
+
+    components: {},
+
+    data(){
+      return {};
+    }
+  }
+
+</script>
+
+
+<!------------------ S T Y L E ------------------>
+
+<style lang="scss" scoped>
+
+  .footer {
+    color: white;
+    background-color: lightsalmon;
+    width: 100%;
+    height: 200px;
+  }
+
+</style>
