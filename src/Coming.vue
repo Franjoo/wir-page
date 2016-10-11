afc8a927740eb3a264fa9b5d0c724a4c53494582
Franz Benthin <fbenthin@mevango.com>
2016-10-11 15:33:44 +0200

    add dev routing

---
+++ src/Coming.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="coming">
+    <span>Coming Soon</span>
+  </div>
+</template>
+
+<style lang="scss">
+
+  html {
+    height: 100%;
+  }
+
+  body {
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    overflow: auto;
+    background-color: #2b2b2b;
+    color: white;
+    font-size: 3em;
+    font-family: Source Sans Pro, Helvetica, sans-serif;
+    font-weight: 100;
+    letter-spacing: 3px;
+
+  }
+
+  #coming {
+
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+  }
+
+</style>
