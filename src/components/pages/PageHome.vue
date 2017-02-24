929323ef6d982707f4e1bb5bc5730df4df6440a2
Franz Benthin <fbenthin@mevango.com>
2017-02-24 14:31:53 +0100

    add soundcloud playlist embed

--- src/components/pages/PageHome.vue
+++ src/components/pages/PageHome.vue
@@ -22,6 +22,12 @@
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/305110278&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&visual=true"></iframe>
     </div>
 
+    <!--soundcloud - playlist-->
+    <div class="entry-ctn">
+    <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2645379&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"></iframe>
+    </div>
+
+
     <div class="entry-ctn">
 
       <countdown
